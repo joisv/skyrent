bcc74c8f2ec4b39393444a9176e6fd20d3705374
joisvvv <joisvvv@hotmail.com>
2025-08-19 22:41:12 +0700

    fix mobile nav

--- resources/views/components/responsif-link.blade.php
+++ resources/views/components/responsif-link.blade.php
@@ -1,25 +1,12 @@
 @props([
     'active' => false,
-    'icon' => null,
 ])
 
-<li class="border-opacity-30 py-2 {{ $active ? 'bg-gray-300' : '' }}">
+<li class=" {{ $active ? 'text-gray-800 font-extrabold' : '' }}">
     <a {{ $attributes->merge([
             'class' => 'block',
             '@click' => 'setNav = false'
         ]) }}>
-        <div class="flex items-center justify-between px-3">
-            <div class="flex items-center space-x-3">
-                @if($icon)
-                    <x-dynamic-component 
-                        :component="$icon" 
-                        class="w-5 h-5 {{ $active ? 'text-blue-600' : 'text-gray-600' }}" 
-                    />
-                @endif
-                <p class="font-semibold text-base {{ $active ? 'text-blue-600' : 'text-gray-600' }}">
-                    {{ $slot }}
-                </p>
-            </div>
-        </div>
+            {{ $slot }}
     </a>
 </li>
--- resources/views/livewire/layout/home-navigation.blade.php
+++ resources/views/livewire/layout/home-navigation.blade.php
@@ -13,10 +13,106 @@
     </div>
     <div>
         <div x-cloak x-data
-            class="flex flex-col lg:hidden w-full max-w-[300px] bg-white h-screen fixed z-50 p-2 right-0 ease-in duration-100"
+            class="flex flex-col lg:hidden w-full  bg-black h-screen fixed z-50 p-2 right-0 ease-in duration-100 top-0"
             :class="{ 'translate-x-full': !setNav, '': setNav }">
+            <div
+                class=" w-full flex flex-col justify-center items-center text-gray-200 h-full text-center text-3xl font-medium ">
+                <div class="space-y-10">
+                    <ul class="space-y-2">
+                        <x-responsif-link :href="route('welcome')" :active="request()->routeIs('welcome')" wire:navigate >
+                            Home
+                        </x-responsif-link>
 
-            <ul class="mt-3">
+                        <x-responsif-link :href="route('products')" :active="request()->routeIs('products')" wire:navigate>
+                            Product
+                        </x-responsif-link>
+
+                        <x-responsif-link href="/" >
+                            Price
+                        </x-responsif-link>
+
+                        <x-responsif-link :href="route('faqs')" :active="request()->routeIs('faqs')" wire:navigate>
+                            FAQ
+                        </x-responsif-link>
+
+                        <x-responsif-link :href="route('contacts')" :active="request()->routeIs('contacts')" wire:navigate>
+                            Contact
+                        </x-responsif-link>
+                    </ul>
+                    <div class="flex items-center space-x-2">
+                        {{-- whatsapp --}}
+                        <a href="{{ $setting->whatsapp }}" target="_blank">
+                            <svg width="28px" height="28px" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path
+                                        d="M6.014 8.00613C6.12827 7.1024 7.30277 5.87414 8.23488 6.01043L8.23339 6.00894C9.14051 6.18132 9.85859 7.74261 10.2635 8.44465C10.5504 8.95402 10.3641 9.4701 10.0965 9.68787C9.7355 9.97883 9.17099 10.3803 9.28943 10.7834C9.5 11.5 12 14 13.2296 14.7107C13.695 14.9797 14.0325 14.2702 14.3207 13.9067C14.5301 13.6271 15.0466 13.46 15.5548 13.736C16.3138 14.178 17.0288 14.6917 17.69 15.27C18.0202 15.546 18.0977 15.9539 17.8689 16.385C17.4659 17.1443 16.3003 18.1456 15.4542 17.9421C13.9764 17.5868 8 15.27 6.08033 8.55801C5.97237 8.24048 5.99955 8.12044 6.014 8.00613Z"
+                                        fill="#e5e7eb"></path>
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M12 23C10.7764 23 10.0994 22.8687 9 22.5L6.89443 23.5528C5.56462 24.2177 4 23.2507 4 21.7639V19.5C1.84655 17.492 1 15.1767 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23ZM6 18.6303L5.36395 18.0372C3.69087 16.4772 3 14.7331 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C11.0143 21 10.552 20.911 9.63595 20.6038L8.84847 20.3397L6 21.7639V18.6303Z"
+                                        fill="#e5e7eb"></path>
+                                </g>
+                            </svg>
+
+                        </a>
+                        {{-- instagram --}}
+                        <a href="{{ $setting->instagram }}" target="_blank">
+                            <svg width="28px" height="28px" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg" stroke="#e5e7eb"
+                                stroke-width="0.00024000000000000003">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
+                                        fill="#e5e7eb"></path>
+                                    <path
+                                        d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"
+                                        fill="#e5e7eb"></path>
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z"
+                                        fill="#e5e7eb"></path>
+                                </g>
+                            </svg>
+                        </a>
+                        {{-- tiktok --}}
+                        <a href="{{ $setting->tiktok }}" target="_blank">
+                            <svg fill="#e5e7eb" width="28px" height="28px" viewBox="0 0 32 32" version="1.1"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <title>tiktok</title>
+                                    <path
+                                        d="M16.656 1.029c1.637-0.025 3.262-0.012 4.886-0.025 0.054 2.031 0.878 3.859 2.189 5.213l-0.002-0.002c1.411 1.271 3.247 2.095 5.271 2.235l0.028 0.002v5.036c-1.912-0.048-3.71-0.489-5.331-1.247l0.082 0.034c-0.784-0.377-1.447-0.764-2.077-1.196l0.052 0.034c-0.012 3.649 0.012 7.298-0.025 10.934-0.103 1.853-0.719 3.543-1.707 4.954l0.020-0.031c-1.652 2.366-4.328 3.919-7.371 4.011l-0.014 0c-0.123 0.006-0.268 0.009-0.414 0.009-1.73 0-3.347-0.482-4.725-1.319l0.040 0.023c-2.508-1.509-4.238-4.091-4.558-7.094l-0.004-0.041c-0.025-0.625-0.037-1.25-0.012-1.862 0.49-4.779 4.494-8.476 9.361-8.476 0.547 0 1.083 0.047 1.604 0.136l-0.056-0.008c0.025 1.849-0.050 3.699-0.050 5.548-0.423-0.153-0.911-0.242-1.42-0.242-1.868 0-3.457 1.194-4.045 2.861l-0.009 0.030c-0.133 0.427-0.21 0.918-0.21 1.426 0 0.206 0.013 0.41 0.037 0.61l-0.002-0.024c0.332 2.046 2.086 3.59 4.201 3.59 0.061 0 0.121-0.001 0.181-0.004l-0.009 0c1.463-0.044 2.733-0.831 3.451-1.994l0.010-0.018c0.267-0.372 0.45-0.822 0.511-1.311l0.001-0.014c0.125-2.237 0.075-4.461 0.087-6.698 0.012-5.036-0.012-10.060 0.025-15.083z">
+                                    </path>
+                                </g>
+                            </svg>
+                        </a>
+                        {{-- facebook --}}
+                        <a href="{{ $setting->facebook }}" target="_blank">
+                            <svg fill="#e5e7eb" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"
+                                width="28px" height="28px" viewBox="0 0 14.906 32">
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <path
+                                        d="M14.874,11.167 L14.262,14.207 C14.062,15.208 13.100,15.992 12.072,15.992 L10.000,15.992 L10.000,30.000 C10.000,31.104 9.159,32.000 8.049,32.000 L5.030,32.000 C3.920,32.000 3.017,31.102 3.017,29.999 L3.017,15.992 L2.011,15.992 C0.901,15.992 -0.002,15.095 -0.002,13.991 L-0.002,10.990 C-0.002,9.887 0.901,8.989 2.011,8.989 L3.017,8.989 L3.017,6.003 C3.017,2.716 5.693,0.041 8.994,0.013 C9.015,0.012 9.033,0.001 9.055,0.001 L13.081,0.001 C13.636,0.001 14.000,0.448 14.000,1.000 L14.000,6.000 C14.000,6.553 13.636,7.004 13.081,7.004 L10.061,7.004 L10.060,8.989 L13.079,8.989 C13.645,8.989 14.167,9.228 14.509,9.644 C14.852,10.059 14.985,10.615 14.874,11.167 ZM9.092,10.990 C9.078,10.991 9.067,10.998 9.053,10.998 L9.053,10.998 C8.497,10.997 8.046,10.549 8.047,9.997 L8.047,9.990 C8.047,9.990 8.047,9.990 8.047,9.990 C8.047,9.990 8.047,9.990 8.047,9.990 L8.049,6.003 C8.049,5.450 8.499,5.003 9.055,5.003 L12.074,5.003 L12.074,2.002 L9.094,2.002 C9.077,2.002 9.063,2.011 9.045,2.011 C6.831,2.011 5.030,3.802 5.030,6.003 L5.030,10.005 C5.030,10.558 4.579,11.006 4.023,11.006 C3.996,11.006 3.973,10.992 3.946,10.990 L2.011,10.990 L2.011,13.991 L4.023,13.991 C4.579,13.991 5.030,14.439 5.030,14.992 C5.030,15.044 5.008,15.088 5.000,15.138 L5.000,30.000 L8.049,29.999 L8.049,15.002 C8.049,14.998 8.047,14.995 8.047,14.992 C8.047,14.439 8.497,13.991 9.053,13.991 L12.072,13.991 C12.145,13.991 12.275,13.886 12.288,13.816 L12.857,10.990 L9.092,10.990 Z">
+                                    </path>
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
+
+                </div>
+                <div class="mt-8 text-2xl font-semibold text-gray-800 text-center border-t border-gray-800 mx-5 pt-5">
+                    <h1>Capture the Moment, Live the Experience</h1>
+                </div>
+            </div>
+
+            {{-- <ul class="mt-3">
 
                 <x-responsif-link :href="route('welcome')" :active="request()->routeIs('welcome')" wire:navigate icon="heroicon-o-home">
                     Home
@@ -38,9 +134,21 @@
                 <x-responsif-link :href="route('contacts')" :active="request()->routeIs('contacts')" wire:navigate icon="heroicon-o-envelope">
                     Contact
                 </x-responsif-link>
-            </ul>
-            <button class="absolute bottom-4 right-0" @click="setNav = false">
-                <x-icons.arrow color="#350B75" />
+            </ul> --}}
+            <button class="absolute top-4 right-4" @click="setNav = false">
+                <div class="flex space-x-3 items-center">
+                    <h3 class="text-gray-200 text-xl font-medium">close</h3>
+                    <svg fill="#e5e7eb" width="20px" height="20px" viewBox="0 0 16 16"
+                        xmlns="http://www.w3.org/2000/svg" stroke="#e5e7eb" stroke-width="0.00016">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path
+                                d="M0 14.545L1.455 16 8 9.455 14.545 16 16 14.545 9.455 8 16 1.455 14.545 0 8 6.545 1.455 0 0 1.455 6.545 8z"
+                                fill-rule="evenodd"></path>
+                        </g>
+                    </svg>
+                </div>
             </button>
         </div>
     </div>
