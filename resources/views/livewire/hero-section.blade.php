99084c2168d45d4a0c3ccb731e51138f18f67385
joisvvv <joisvvv@hotmail.com>
2026-01-19 00:47:36 +0700

    hero section ui redisign

--- resources/views/livewire/hero-section.blade.php
+++ resources/views/livewire/hero-section.blade.php
@@ -1,5 +1,5 @@
 <div class="relative w-full h-auto" x-data="{ openBottomSheet: false }">
-    <div
+    {{-- <div
         class="flex flex-col lg:flex-row lg:items-center lg:justify-center lg:space-x-6 max-w-screen-2xl mx-auto space-y-8">
         <!-- Teks & Filter -->
         <div class="w-full lg:w-1/2 md:space-y-9 p-3 lg:p-5 ">
@@ -64,5 +64,49 @@
             loop: true,
             loopedSlides: 5,
         });
-    </script>
+    </script> --}}
+    <section class="max-w-screen-2xl mx-auto px-6 py-10">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-12 items-start">
+
+            <!-- LEFT CONTENT -->
+            <div class="flex flex-col items-center lg:items-start w-full ">
+                <div class="flex flex-col justify-center items-center lg:items-start">
+
+                    <!-- Badge -->
+                    <div
+                        class="inline-block mb-6 px-4 py-1 text-sm font-medium border rounded-full
+                mx-auto lg:mx-0">
+                        Skyrental™
+                    </div>
+
+                    <!-- Heading -->
+                    <h1
+                        class="text-5xl md:text-6xl font-extrabold leading-tight mb-6
+               text-center lg:text-left">
+                        <span class="bg-gradient-to-r from-orange-400 to-orange-500 bg-clip-text text-transparent">
+                            Sewa iPhone
+                        </span><br>
+                        <span class="bg-gradient-to-r from-orange-500 to-pink-500 bg-clip-text text-transparent">
+                            no.1 di Banyuwangi
+                        </span>
+                    </h1>
+
+                </div>
+
+
+                <p class="text-gray-600 mb-8 text-center lg:text-left md:text-lg">
+                    {{ $setting->tagline }}
+                </p>
+                <div class="w-full">
+                    <livewire:filter-section />
+                </div>
+            </div>
+
+            <!-- RIGHT IMAGE -->
+            <div class="flex justify-center lg:justify-end -z-50">
+                <img src="{{ url('ip17.png') }}" alt="iPhone" class="max-w-48 md:max-w-md drop-shadow-2xl" />
+            </div>
+
+        </div>
+    </section>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,34 @@
 <x-home>
-    <livewire:hero-section/>
-    <livewire:review-page/>
+    <livewire:hero-section />
+    <livewire:review-page />
+    <section class="overflow-hidden py-5 lg:py-2 bg-white space-y-2 ">
+
+        <!-- TOP : Right to Left -->
+        <div class="relative overflow-hidden">
+            <div class="flex w-max animate-marquee-left gap-16">
+                <span class="text-[10vw] font-extrabold text-gray-900 whitespace-nowrap">
+                    Sewa iPhone no.1 😎
+                </span>
+                <span class="text-[10vw] font-extrabold text-gray-900 whitespace-nowrap">
+                    Sewa iPhone no.1
+                </span>
+            </div>
+        </div>
+
+        <!-- BOTTOM : Left to Right -->
+        <div class="relative overflow-hidden">
+            <div class="flex w-max animate-marquee-right gap-16">
+                <span class="text-[10vw] font-extrabold text-transparent stroke-text whitespace-nowrap">
+                    Raya Di Banyuwangi<span class="text-orange-500">.</span>
+                </span>
+                <span class="text-[10vw] font-extrabold text-transparent stroke-text whitespace-nowrap">
+                    Raya Di Banyuwangi<span class="text-orange-500">.</span>
+                </span>
+            </div>
+        </div>
+
+    </section>
     <div class="max-w-screen-2xl mx-auto">
-        <livewire:cards lazy="on-load"/>
+        <livewire:cards lazy="on-load" />
     </div>
 </x-home>
